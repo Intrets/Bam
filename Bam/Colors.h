31b4f6fa4f6a95084dc19ff6cfede800d5708afc
Ik Mac <azarintrets@live.nl>
2020-08-17 11:00:18 +0200

    hex color literal conversion fixed

--- Bam/Colors.h
+++ Bam/Colors.h
@@ -5,10 +5,10 @@ constexpr uint32_t char_to_hex(const char c) {
 		return c - '0';
 	}
 	else if (c >= 'A' && c <= 'F') {
-		return c - 'A';
+		return c - 'A' + 10;
 	}
 	else if (c >= 'a' && c <= 'f') {
-		return c - 'a';
+		return c - 'a' + 10;
 	}
 	else {
 		assert(0);
@@ -17,22 +17,20 @@ constexpr uint32_t char_to_hex(const char c) {
 };
 
 constexpr uint32_t operator"" _hex(const char* c, std::size_t size) {
-	uint32_t r = 0, g = 0, b = 0, a = 0;
+	int32_t hex = 0;
 	int32_t offset = 0;
 	if (size == 7 || size == 6) {
-		a = 0xff;
+		hex = 0xff;
 	}
 	else {
-		a = (char_to_hex(c[size - 2 - offset]) << 4) + char_to_hex(c[size - 1 - offset]);
+		hex = (char_to_hex(c[size - 2 - offset]) << 4) + char_to_hex(c[size - 1 - offset]);
 		offset += 2;
 	}
-	b = (char_to_hex(c[size - 2 - offset]) << 4) + char_to_hex(c[size - 1 - offset]);
-	offset += 2;
-	g = (char_to_hex(c[size - 2 - offset]) << 4) + char_to_hex(c[size - 1 - offset]);
-	offset += 2;
-	r = (char_to_hex(c[size - 2 - offset]) << 4) + char_to_hex(c[size - 1 - offset]);
 
-	return (r << 24) + (g << 16) + (b << 8) + a;
+	for (int32_t i = 0; i < 6; i++) {
+		hex += char_to_hex(c[size - i - 1 - offset]) << (4 * (i + 2));
+	}
+	return hex;
 };
 
 namespace COLORS
@@ -49,11 +47,8 @@ namespace COLORS
 
 	glm::vec4 DARKEN(glm::vec4 color);
 
-	//static glm::vec4 TEXT = hex("#dec165"_hex);
-	static glm::vec4 TEXT = hex(0xdec165);
-	//static glm::vec4 BACKGROUND = hex("#251200"_hex);
-	static glm::vec4 BACKGROUND = hex(0x251200);
-	//static glm::vec4 FOREGROUND = hex("#e55a1c"_hex);
-	static glm::vec4 FOREGROUND = hex(0xe55a1c);
+	static glm::vec4 TEXT = hex("#dec165"_hex);
+	static glm::vec4 BACKGROUND = hex("#25120z"_hex);
+	static glm::vec4 FOREGROUND = hex("#e55a1c"_hex);
 	static glm::vec4 FOCUSSED = rgb(242, 242, 242);
 }
